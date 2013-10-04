c82c9fccd44958f47019bb8c4b2623ce77d602c1
kos89 <sviridov.kos@rambler.ru>
2013-10-04 17:22:56 +0700

    1

--- TasksDevite/TasksDevite/Forms/TaskMonthForm.Designer.cs
+++ TasksDevite/TasksDevite/Forms/TaskMonthForm.Designer.cs
@@ -29,16 +29,62 @@
         private void InitializeComponent()
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ColumnAdd = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ColumnUser = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnClient = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnStart = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEnd = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView
             // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnAdd,
+            this.ColumnUser,
+            this.ColumnClient,
+            this.ColumnDate,
+            this.ColumnStart,
+            this.ColumnEnd});
             this.dataGridView.Location = new System.Drawing.Point(12, 12);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.Size = new System.Drawing.Size(878, 376);
             this.dataGridView.TabIndex = 0;
+            // 
+            // ColumnAdd
+            // 
+            this.ColumnAdd.HeaderText = "Добавить";
+            this.ColumnAdd.Name = "ColumnAdd";
+            // 
+            // ColumnUser
+            // 
+            this.ColumnUser.HeaderText = "Ответственный";
+            this.ColumnUser.Name = "ColumnUser";
+            // 
+            // ColumnClient
+            // 
+            this.ColumnClient.HeaderText = "Клиент";
+            this.ColumnClient.Name = "ColumnClient";
+            // 
+            // ColumnDate
+            // 
+            this.ColumnDate.HeaderText = "Дата";
+            this.ColumnDate.Name = "ColumnDate";
+            // 
+            // ColumnStart
+            // 
+            this.ColumnStart.HeaderText = "Начало";
+            this.ColumnStart.Name = "ColumnStart";
+            // 
+            // ColumnEnd
+            // 
+            this.ColumnEnd.HeaderText = "Конец";
+            this.ColumnEnd.Name = "ColumnEnd";
             // 
             // TaskMonthForm
             // 
@@ -57,5 +103,11 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnUser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnClient;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnStart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEnd;
     }
 }
--- TasksDevite/TasksDevite/Forms/TaskMonthForm.cs
+++ TasksDevite/TasksDevite/Forms/TaskMonthForm.cs
@@ -2,6 +2,8 @@
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using DBHelper;
+using System.Collections.Generic;
+using System;
 
 namespace TasksDevite
 {
@@ -10,25 +12,35 @@ namespace TasksDevite
         public TaskMonthForm()
         {
             InitializeComponent();
+            gridReload();
         }
 
         private void gridReload()
         {
+            string days;
             SqlConnection cn = new SqlConnection();
             try
             {
                 cn = DBDevite.DBOpen();
 
-                SqlCommand command = new SqlCommand("SELECT t.ID, u.Users as Фамилия, c.Name as Клиент, c.Days, t.TimeStart as Начало, t.TimeEnd as Конец, t.TaskStatus as Статус " +
-                                                   "FROM clients c " +
-                                                   "LEFT JOIN users u ON c.userID = u.ID", cn);
+                SqlCommand command = new SqlCommand("SELECT u.Users, c.Name, c.Days, c.TimeStart, c.TimeEnd, c.ClientStatus " +
+                                                    "FROM clients c " +
+                                                    "LEFT JOIN users u ON c.userID = u.ID", cn);
                 SqlDataReader reader = command.ExecuteReader();
 
                 while (reader.HasRows)
                 {
                     while (reader.Read())
                     {
-
+                        days = reader.GetString(2).Trim();
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (days[i] == '1')
+                                dataGridView.Rows.Add(true, reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
+                                //MessageBox.Show("d");
+                        }
+                            //MessageBox.Show(days.Trim());
+                            //dataGridView.Rows.Add(true, reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
                     }
                     reader.NextResult();
                 }
@@ -41,6 +53,13 @@ namespace TasksDevite
             {
                 DBDevite.DBClose(cn);
             }
+        }
+
+        private List<DateTime> dateToDays(DateTime dt)
+        {
+            List<DateTime>  dates = new List<DateTime>();
+
+            return da;
         }
     }
 }
